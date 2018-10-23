03a33776423af3733bf31397642c8588ae225a83
ninjawarrior1337 <t.rothled@gmail.com>
2018-10-22 23:47:09 -0700

    added about command, avatar command, and patrick command

---
+++ commands/utils/about.js
@@ -0,0 +1,29 @@
+const {Command} = require("discord.js-commando")
+const {RichEmbed} = require('discord.js')
+
+class about extends Command
+{
+    constructor(client)
+    {
+        super(client, {
+            description: "Shows information about the bot.",
+            name: "about",
+            group: "utils",
+            memberName: "about",
+            aliases: ["about", "info", "introduce", "introduction", "intro"]
+        })
+    }
+
+    async run(msg)
+    {
+        var embed = new RichEmbed()
+            .setColor(0x3399ff)
+            .setTitle("こんにちは (Hello), I am Tyler's discord bot...")
+            .setDescription("I am a very useful, [open source](http://github.com/ninjawarrior1337/crocs-and-socks), and very much a weeb bot.")
+            .setURL("http://github.com/ninjawarrior1337/crocs-and-socks")
+            .setThumbnail(this.client.user.avatarURL)
+        msg.channel.send(embed)
+    }
+}
+
+module.exports = about
